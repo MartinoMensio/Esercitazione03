ff5bdaf5965c8a9cd5ae271d4fad1ab0b9c2d17b
Martino Mensio <martinomensio@outlook.it>
2017-04-21 16:50:37 +0200

    stub of first point
    todo

---
+++ PostGisDBCreator/src/main/java/it/polito/ai/lab3/Main.java
@@ -0,0 +1,19 @@
+package it.polito.ai.lab3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		 * steps:
+		 * - connect to postgis db (lab2)
+		 * - drop table BusStopGeographic if exists
+		 * - create table BusStopGeographic with proper schema (geographic points instead of lat+lng)
+		 * - insert each row from BusStop to BusStopGeographic
+		 * 
+		 */
+
+	}
+
+}
