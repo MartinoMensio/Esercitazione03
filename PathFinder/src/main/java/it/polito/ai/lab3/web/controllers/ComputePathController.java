9a4e43bd39c3c540b4a152042cc7df827ee0725b
aleciav <ale.ciav92@gmail.com>
2017-04-30 13:32:16 +0200

    POST-then-GET paradigm and form validation added
    There is a small issue when refreshing the page shown after the redirect: the result disappears

--- PathFinder/src/main/java/it/polito/ai/lab3/web/controllers/ComputePathController.java
+++ PathFinder/src/main/java/it/polito/ai/lab3/web/controllers/ComputePathController.java
@@ -1,18 +1,66 @@
+/**
+ * This class implements the POST-then-GET paradigm
+ * 
+ * @author alessio
+ */
+
 package it.polito.ai.lab3.web.controllers;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import it.polito.ai.lab3.services.routing.Path;
+import it.polito.ai.lab3.services.routing.RoutingService;
+import it.polito.ai.lab3.web.forms.SrcDstPointsForm;
+
 
 @Controller
 @RequestMapping("/computePath")
 public class ComputePathController {
+	private RoutingService routingService;
+	
+	
+	@Autowired
+	public ComputePathController(RoutingService routingService) {
+		this.routingService = routingService;
+	}
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public String showResultPage(ModelMap model) {
+		// set an empty SrcDstPointsForm object to be used for the POST request
+		model.addAttribute("srcDstPoints", new SrcDstPointsForm());
 		return "computePath";
 	}
 	
-	// TODO verificare la possibilità di usare la stessa pagina al posto di result Path
+	@RequestMapping(method = RequestMethod.POST)
+	public String computePage(@Valid @ModelAttribute("srcDstPoints") SrcDstPointsForm srcDstPoints, BindingResult result, RedirectAttributes ras) {
+		// validate received data
+		if(result.hasErrors()) {
+			// If there are errors then show again the previous page
+			return "computePath";
+		 }
+		
+		// Compute the path from the source point to the destination point
+		Path path = routingService.getPath(srcDstPoints.getSrcLat(), 
+										   srcDstPoints.getSrcLng(), 
+										   srcDstPoints.getDstLat(), 
+										   srcDstPoints.getDstLng());
+		
+		// Add the results for the view
+		ras.addFlashAttribute("path", path.getAllBusStopsAsGeoJson());
+		
+		/*
+		 * Redirect to the resultPath page
+		 * Warning! do not put space after ":" otherwise the flash attributes are not passed to the new page
+		 */
+		return "redirect:resultPath"; 
+	}
 }
--- PathFinder/src/main/java/it/polito/ai/lab3/web/controllers/ResultPathController.java
+++ PathFinder/src/main/java/it/polito/ai/lab3/web/controllers/ResultPathController.java
@@ -1,33 +1,16 @@
 package it.polito.ai.lab3.web.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import it.polito.ai.lab3.services.routing.Path;
-import it.polito.ai.lab3.services.routing.RoutingService;
 
 @Controller
 @RequestMapping("/resultPath")
 public class ResultPathController {
-	private RoutingService routingService;
 	
-	
-	@Autowired
-	public ResultPathController(RoutingService routingService) {
-		this.routingService = routingService;
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	public String showResult(ModelMap model) {
-		//TODO Add info to the model, i.e. the path and other info
-		
-		Path path = routingService.getPath("", "", "", "");
-		model.addAttribute("path", path.getAllBusStopsAsGeoJson());
-		
-		
+	@RequestMapping(method = RequestMethod.GET)
+	public String showResult() {
 		return "resultPath";
 	}
 }
