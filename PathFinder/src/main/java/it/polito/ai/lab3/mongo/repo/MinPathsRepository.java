5c4218e84a52ef5f0162388b9d95d542ae70b543
Martino Mensio <martinomensio@outlook.it>
2017-05-05 12:50:47 +0200

    warnings removed

--- PathFinder/src/main/java/it/polito/ai/lab3/mongo/repo/MinPathsRepository.java
+++ PathFinder/src/main/java/it/polito/ai/lab3/mongo/repo/MinPathsRepository.java
@@ -3,14 +3,9 @@ package it.polito.ai.lab3.mongo.repo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import it.polito.ai.lab3.mongo.repo.entities.Key;
